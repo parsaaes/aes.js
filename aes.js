9e2e83412ebc2926a9d7305f99f913df568062c8
Parsa Eskandarnejad <parsaaes@gmail.com>
2020-08-31 12:47:04 +0430

    Improve code style

--- aes.js
+++ aes.js
@@ -2,7 +2,7 @@
 async function digest(message) {
   const encoder = new TextEncoder();
   const data = encoder.encode(message);
-  const hash = await crypto.subtle.digest("SHA-256", data);
+  const hash = await window.crypto.subtle.digest("SHA-256", data);
   return hash;
 }
 
